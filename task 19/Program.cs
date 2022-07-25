1e6dba51eb135cbc865a56825bf08365ce64079e
Виталий Иванов <shahmatis@example.com>
2022-07-25 19:13:28 +0300

    сделал третие д/з

---
+++ task 19/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+
+void CheckingNumber(string number){
+ if (number[0]==number[4] || number[1]==number[3]){ //использую логическое сокращённое или  если одно из условий true , то палиндром  иначе нет //
+ Console.WriteLine($"Ваше число: {number} - палиндром.");
+  }
+ else Console.WriteLine("Ваше число: {number} - НЕ палиндром.");
+}
+
+if (number!.Length == 5){ // ограничиваю чтоб пятизначное вводил//
+ CheckingNumber(number);
+} else Console.WriteLine("Введи правильное число");
+
